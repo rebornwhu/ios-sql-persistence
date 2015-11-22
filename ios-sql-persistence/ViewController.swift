7d197dddfac262f3788170b5b86099a473041b0f
rebornwhu <rebornwhu@gmail.com>
2015-11-21 20:33:37 -0800

    init commit

---
+++ ios-sql-persistence/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ios-sql-persistence
+//
+//  Created by Xiao Lu on 11/21/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var lineFields:[UITextField]!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ ios-sql-persistenceTests/ios_sql_persistenceTests.swift
@@ -0,0 +1,36 @@
+//
+//  ios_sql_persistenceTests.swift
+//  ios-sql-persistenceTests
+//
+//  Created by Xiao Lu on 11/21/15.
+//  Copyright © 2015 Xiao Lu. All rights reserved.
+//
+
+import XCTest
+@testable import ios_sql_persistence
+
+class ios_sql_persistenceTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
